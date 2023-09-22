0954aaaadf7e1722b48df1ba474ed092fa1b1374
Èric Salat <ericsalat@protonmail.com>
2023-09-22 11:40:46 +0200

    Mejorada la lógica de renderizar el componentr Track

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -2,19 +2,34 @@ import * as React from "react";
 import Track from "../components/Track";
 
 function TrackList() {
+
+    const albumData = [
+        { id: 1, trackNumber: "1", trackName: "Prelude", trackTime: "1:03" },
+        { id: 2, trackNumber: "3", trackName: "Bad Ideas", trackTime: "2:59" },
+        { id: 3, trackNumber: "2", trackName: "Crush", trackTime: "3:36" },
+        { id: 4, trackNumber: "4", trackName: "I Like (the idea of) You", trackTime: "2:48" },
+        { id: 5, trackNumber: "5", trackName: "Games", trackTime: "3:41" },
+        { id: 6, trackNumber: "6", trackName: "Feelin", trackTime: "3:04" },
+        { id: 7, trackNumber: "7", trackName: "Words Ain't Enough", trackTime: "2:04" },
+        { id: 8, trackNumber: "8", trackName: "Bored", trackTime: "3:03" },
+        { id: 9, trackNumber: "9", trackName: "Wishful Drinking", trackTime: "3:15" },
+        { id: 10, trackNumber: "10", trackName: "Honest", trackTime: "4:12" },
+        { id: 11, trackNumber: "11", trackName: "Interlude III", trackTime: "2:49" }
+      ];
+
     return(
         <table className="table">
-            <Track trackNumber="1" trackName="Prelude" trackTime="1:03"/>
-            <Track trackNumber="2" trackName="Crush" trackTime="3:36"/>
-            <Track trackNumber="3" trackName="Bad Ideas" trackTime="2:59"/>
-            <Track trackNumber="4" trackName="I Like (the idea of) You" trackTime="2:48"/>
-            <Track trackNumber="5" trackName="Games" trackTime="3:41"/>
-            <Track trackNumber="6" trackName="Feelin" trackTime="3:04"/>
-            <Track trackNumber="7" trackName="Words Ain't Enough" trackTime="2:04"/>
-            <Track trackNumber="8" trackName="Bored" trackTime="3:03"/>
-            <Track trackNumber="9" trackName="Wishful Drinking" trackTime="3:15"/>
-            <Track trackNumber="10" trackName="Honest" trackTime="4:12"/>
-            <Track trackNumber="11" trackName="Interlude III" trackTime="2:49"/>
+
+            {albumData.map((data) => (
+                 <Track key={data.id}
+                 
+                        trackNumber={data.trackNumber}
+                        trackName={data.trackName}
+                        trackTime={data.trackTime}
+                />
+            ))}
+
+            
         </table>
     ); 
 }
